919eced66b7a88943159babdd23b3e663aa84082
Sebastian Arias <sebastianrobayog@gmail.com>
2021-11-08 20:52:11 -0500

    Actualización de archivos y finalización del login

--- server/routes.js
+++ server/routes.js
@@ -16,14 +16,8 @@ routes.get('/', (req,res) => {
 routes.post('/', (req,res) => {
     req.getConnection((err, conn) => {
         if(err) return res.send(err);
-        console.log("Connected!");
 
-        let titulo = req.body.titulo;
-        let imagen = req.body.imagen;
-        let descripcion = req.body.descripcion;
-        let precio = req.body.precio;
-
-        conn.query(`INSERT INTO productos (titulo,imagen,descripcion,precio) VALUES ('${titulo}','${imagen}','${descripcion}','${precio}')`, [req.body], (err, rows) => {
+        conn.query('INSERT INTO productos set ?', [req.body], (err, rows) => {
             if(err) return res.send(err);
 
             res.send('Articulo agregado');
